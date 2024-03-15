e21875ac0ad823df143f1e94afa865bbfef44c73
Flotschi <tanicruzzer2@gmail.com>
2024-03-15 16:01:39 +0100

    embedded beauty

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Flotschis Svelte page" />
+    <title>Home</title>
+    <meta name="description" content="Flotschis Svelte page" />
 </svelte:head>
 
 <section>
     <h1>This is Flotschis Site</h1>
     <p>I am looking forward to use this as a playground</p>
+    <iframe
+        src="http://www.staggeringbeauty.com/"
+        style="border: 1px inset #ddd"
+        width="498"
+        height="598"
+    ></iframe>
 </section>
